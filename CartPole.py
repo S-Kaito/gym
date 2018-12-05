e7c7e6f20a26fa0302ba0c1f3910c7bf92ecb5e7
S-Kaito <irc.darjeeling@gmail.com>
2018-12-05 23:32:56 +0900

    2018_1205 23:32

--- CartPole.py
+++ CartPole.py
@@ -1,61 +1,95 @@
 import gym
+from collections import deque
+import keras
+from keras import optimizers
+from keras import losses
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras.utils import plot_model
+from keras import backend as K
+import matplotlib.pyplot as plt
 import numpy as np
 import random
 import sys
-import TensowFlow as tf
+import time
 
-EPOCH = 100
-BATCH_SIZE = 20
-CLEAR_TURN = 50
+EPOCH = 200
+BATCH_SIZE = 32
+CLEAR_TURN = 100
 GAMMA = 0.99
 
-env = gym.make('CartPole-v0')
-network = tf.Network([tf.LayerLeakyReLU(4,15),tf.LayerIdentity(15,2)])
-data = []
-
-def getAction(observation,episode):
-	y = network.forward(observation)
+def getAction(model,observation,episode):
+	y = model.predict(observation.reshape((1,4)))
 	if (0.01 +0.9/(1.0 + episode)) <= np.random.uniform(0,1):
 		return np.argmax(y)
 	else:
 		return np.random.choice([0, 1])
 
-def learn():
-	global network,data
-	newNetwork = network.copy()
-	random.shuffle(data)
+def learn(model,data):
+	y_pred = []
+	y_true = []
 	for d in data:
 		state,action,reward,nextState = d
-		y = np.copy(network.forward(state))
-		y[0][action] = reward + GAMMA * np.max(network.forward(nextState)[0])
-		newNetwork.forward(state)
-		newNetwork.backward(y)
-		newNetwork.update()
-	network = newNetwork
-	data = []
+		y = model.predict(state)
+		if not (nextState == np.zeros(state.shape)).all(axis=1):
+			y[0][action] = reward + GAMMA * np.max(model.predict(nextState)[0])
+		else:
+			y[0][action] = reward
+		y_pred.append(state.reshape(4))
+		y_true.append(y.reshape(2))
+	model.fit(np.array(y_pred),np.array(y_true),batch_size=BATCH_SIZE,verbose=0,epochs=1)
+	
+def huberloss(y_true, y_pred):
+    return K.mean(K.minimum(0.5*K.square(y_pred-y_true), K.abs(y_pred-y_true)-0.5), axis=1)
 
 def main(args):
-	avg = 0
+
+	env = gym.make('CartPole-v0')
+	model = Sequential()
+	model.add(Dense(16,activation="relu",input_dim=4))
+	model.add(Dense(16,activation="relu"))
+	model.add(Dense(2,activation="linear"))
+	model.compile(loss=huberloss, optimizer=Adam(lr=0.00001))
+	model.summary()
+
+	data = deque(maxlen=200)
+
+	plot_x = []
+	plot_y = []
+
+	point = 0
+
 	for episode in range(EPOCH):
 		observation = env.reset()
+		nextObservation, reward, done, info = env.step(env.action_space.sample())
+
 		for t in range(CLEAR_TURN * 2):
-			action = getAction(observation,episode)
+
+			if EPOCH - episode == 2:
+				env.render()
+				time.sleep(0.1)
+
+			action = getAction(model,observation,episode)
 			nextObservation, reward, done, info = env.step(action)
-			print(action)
 			if done:
 				if t > CLEAR_TURN:
 					data.append((observation.reshape((1,4)),action,1,np.zeros((1,4))))
 				else:
 					data.append((observation.reshape((1,4)),action,-1,np.zeros((1,4))))
-				print("finished after {} timestamps".format(t+1))
-				avg += 1
+				print("{} times : finished after {} timestamps".format(episode,t+1))
+	
+				plot_x.append(episode)
+				plot_y.append(t + 1)
+
 				break
 			else:
 				data.append((observation.reshape((1,4)),action,0,nextObservation.reshape((1,4))))
 			observation = nextObservation
 		if BATCH_SIZE < len(data):
-			print("Avg = {} ".format(len(data) / avg),len(data))
-			avg = 0
-			learn()
+			learn(model,data)
+
+	plt.scatter(plot_x,plot_y,marker="+")
+	plt.show()
 
 main(sys.argv)
